2d20bd706e9b9e255354bda8087e8e8756b83e98
Stefma <60974403+Stefma4Git@users.noreply.github.com>
2026-09-22 01:22:25 +0200

    Update books.js

--- books.js
+++ books.js
@@ -21,21 +21,21 @@ const books = [
   {
   title: "U-portazi-za-Judom",
   year: "2026",
-  description: "Sa užasom je razumeo da postoje ljudi koji na dnevnom nivou izdaju Hrista i za mnogo manje para. Proklinjemo Judu, a okruženi smo hiljadama onih koji su daleko jeftiniji od njega. ",
+  description: "Alegorično delo koje govori o propasti jednog naselja zvanog Sloboda. Napuštajući domove u potrazi za slobodom ljudi postaju robovi savremenog vrednosnog sistema.",
  pdf: "books/u-potrazi-za-judom.pdf",
   cover: "covers/u-potrazi-za-judom.jpg"
   },
   {
     title: "Dekodiranje-stereotipa",
-    year: "2026",
-    description: "Ima li išta žalosnije od slike čoveka koji je prestao da se bori za sopstvenu slobodu? Ima li šta tužnije od osobe koja izgara boreći se za tuđ ideal slobode? Kako se oseća neko kada shvati da je sagoreo boreći se samo za novi oblik ropstva?",
+    year: "2025",
+    description: "Živimo u matriksu. Ova knjiga skida paravan i pokazuje kako stvari zaista stoje.",
     pdf: "books/Dekodiranje-stereotipa.pdf",
     cover: "covers/Dekodiranje-stereotipa.jpg"
   },
   {
     title: "Kako-je-propala-sloboda",
     year: "2026",
-    description: "Kratak opis druge knjige.",
+    description: "Alegorično delo koje govori o propasti jednog naselja zvanog Sloboda. Napuštajući domove u potrazi za slobodom ljudi postaju robovi savremenog vrednosnog sistema.",
     pdf: "books/Tvoj-neko.pdf",
     cover: "covers/Tvoj-neko.jpg"
   }
